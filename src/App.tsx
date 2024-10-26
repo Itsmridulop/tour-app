5f7385af4e85d5faca0502162cd1554816b90e00
Itsmridulop <mridulmishra2117@gmail.com>
2024-10-25 22:13:26 -0400

    Add AppLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import AppLayout from './component/AppLayout'
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/signup',
+        element: <Signup />,
+      },
+      {
+        path: '/login',
+        element: <Login />
+      }
+    ]
+  }
+])
+
 function App() {
   return (
-    <div>hi</div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/component/Navbar.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+  return (
+    <nav className="flex justify-between items-center bg-zinc-700 m-6 px-8 py-4 text-white">
+      
+      <div className="flex-1">
+        <Link to="/">
+          ALL TOURS
+        </Link>
+      </div>
+
+      <div className="flex-1 flex justify-center">
+        <div >
+          <img src="src/public/img/logo-white.png" alt="Logo" className="h-12 p-1" />
+        </div>
+      </div>
+
+      <div className="flex-1 flex justify-end items-center space-x-6">
+        <Link to="/login">
+          LOG IN
+        </Link>
+        <Link
+          to="/signup"
+          className="border border-white rounded-full py-2 px-6 hover:bg-white hover:text-gray-800 transition duration-300"
+        >
+          SIGN UP
+        </Link>
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
